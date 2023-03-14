83035db3b6cf16bedc73a49ea695b98cd89db627
gauravaryavrat <124332562+gauravaryavrat@users.noreply.github.com>
2023-03-14 12:28:05 +0530

    Update environment.staging.ts

--- client/src/environments/environment.staging.ts
+++ client/src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  appBaseUrl: `http://192.168.1.178`,
-  apiBaseUrl: `http://192.168.1.178/api`,
+  appBaseUrl: `http://13.39.86.195/`,
+  apiBaseUrl: `http://13.39.86.195/api`,
   production: false
 };
